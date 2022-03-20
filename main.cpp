4c3a7bc8d0047352cd93cdd1c3044be9d1901ffa
C Raj Dheeraj Reddy <73807445+Blubird333@users.noreply.github.com>
2022-03-20 10:18:17 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Size of char:"<<int(sizeof(char))<<"byte ";
+    cout<<"Size of int:"<<int(sizeof(int))<<"bytes ";
+    cout<<"Size of float:"<<int(sizeof(float))<<"bytes ";
+    cout<<"Size of double:"<<int(sizeof(double))<<"bytes ";
+
+    return 0;
+}
